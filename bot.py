3853212b758606eaed75ab6d5330924a6749485e
dlmateogarcialopez <dl.mgarcia@umanizales.edu.co>
2022-12-03 11:28:34 -0500

    se agrego fallback

--- bot.py
+++ bot.py
@@ -5,12 +5,24 @@ from time import sleep
 import re
 #########################################################
 # Aquí vendrá la implementación de la lógica del bot
-@bot.message_handler(commands=['ping'])
-def on_command_ping(message):
+@bot.message_handler(commands=['start'])
+def on_command_start(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+
     bot.send_message(
         message.chat.id,
-        "pongg",
+        "Hola, soy un \U0001F929, ¿cómo estás?",
         parse_mode="Markdown") 
+
+@bot.message_handler(func=lambda message: True)
+def on_fallback(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+    bot.reply_to(
+        message,
+        "\U0001F63F Ups, no entendí lo que me dijiste.")
+
 #########################################################
 if __name__ == '__main__':
     bot.polling(timeout=20)
