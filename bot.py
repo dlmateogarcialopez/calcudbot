c931353039db49e47274747e8ec97a67703d42c1
dlmateogarcialopez <dl.mgarcia@umanizales.edu.co>
2022-12-03 09:59:59 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+#########################################################
+from config import bot
+import config
+from time import sleep
+import re
+#########################################################
+# Aquí vendrá la implementación de la lógica del bot
+#########################################################
+if __name__ == '__main__':
+bot.polling(timeout=20)
+#########################################################
